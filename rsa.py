5be272111d902b278f86b63539fbcca46629dbc8
mostahidhasan2019331070 <fahimmostahid70@gmail.com>
2024-04-29 06:50:10 +0600

    edit rsa.py

--- rsa.py
+++ rsa.py
@@ -128,6 +128,6 @@ def main():
    test_rsa(16, "BUETCSEVSSUSTCSE")
    test_rsa(32, "BUETCSEVSSUSTCSE")
    test_rsa(64, "BUETCSEVSSUSTCSE")
-   test_rsa(96, "BUETCSEVSSUSTCSE")
+  
 if __name__ == "__main__":
     main()
